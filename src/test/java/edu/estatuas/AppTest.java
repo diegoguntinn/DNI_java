0b4b82b8d14660ad690a846b9930510244fbd539
diegoguntinn <diego232005@gmail.com>
2025-02-14 12:38:05 +0100

    Corrigiendo finales de línea en .gitignore

---
+++ src/test/java/edu/estatuas/AppTest.java
@@ -0,0 +1,11 @@
+package edu.estatuas;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Test {
+    @Test
+    public void dni_test() {
+        assertEquals("todo bien", "todo bien");
+    }
+}
